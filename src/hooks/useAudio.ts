83b0b8803d48ee7cb5447101044c1000f9d9950a
王耀 <93106545+yy1486578543@users.noreply.github.com>
2023-11-14 11:24:59 +0800

    fix: useAudio hooks return

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,111 +1,208 @@
 import { ref } from 'vue'
 import { useMusicStore } from '@/store/modules/music'
+import { formatMusicLyrics } from '@/utils/handle-lyrics'
+import { useAppStore } from '@/store/modules/app'
+import { getImgColor, getDarkColor } from '@/utils/img'
 import { useToast } from '@/components/Toast'
+import { musicApi } from '@/api/music'
 
 export const audio = ref<HTMLAudioElement | null>(null)
 let timeupdate: EventListenerOrEventListenerObject
 
 export const useAudio = () => {
+  /**
+   *
+   *  创建audio
+   * @param {string} src
+   * @returns
+   * @memberof Audio
+   */
+  const createAudio = (src: string) => {
+    // 清空之前的audio
+    if (audio.value) {
+      audio.value.removeEventListener('ended', audioEnded)
+      audio.value.removeEventListener('timeupdate', timeupdate)
+      audio.value.oncanplay = null
+      audio.value.pause()
+      audio.value = null
+    }
+    audio.value = new Audio(src)
+    console.log('audio', audio.value)
+    console.log('src', src)
+    // 音乐无法播放的情况
+    audio.value.onerror = (err) => {
+      // 重新赋值src
+      // const musicStore = useMusicStore()
+      useToast().open('音乐无法播放')
+      // 下一首
+      // setTimeout(() => {
+      //   if (musicStore.musicList.length > 1) {
+      //     useToast().open('音乐无法播放，自动播放下一首')
+      //     musicStore.nextMusic()
+      //   }
+      // }, 2500)
+    }
+    audio.value.addEventListener('ended', audioEnded)
+    audio.value.addEventListener('timeupdate', timeupdate)
+    audio.value.oncanplay = () => {
+      audioPlay()
+    }
+  }
+  /**
+   * createTimeupdate
+   * @param {Function} fn
+   */
+  const createTimeupdate = (fn: EventListenerOrEventListenerObject) => {
+    timeupdate = fn
+  }
+  /**
+   * 音频播放结束时触发
+   */
+  const audioEnded = () => {
+    // audioPause()
+    const musicStore = useMusicStore()
+    switch (musicStore.playMode) {
+      case 0:
+        // 顺序
+        musicStore.nextMusic()
+        break
+      case 1:
+        // 随机
+        const randomIndex = Math.floor(Math.random() * musicStore.musicList.length)
+        // 防止随机到当前音乐
+        if (randomIndex === musicStore.nowIndex) {
+          musicStore.nextMusic()
+          break
+        }
+        musicStore.changeIndex(randomIndex)
+        break
+      case 2:
+        // 单曲循环
+        audioPlay()
+        break
+      default:
+        break
+    }
+  }
+  /**
+   * audio播放
+   */
+  const audioPlay = () => {
+    audio.value?.play()
+    const dom: HTMLDivElement | null = document.querySelector('.music-img')
+    if (dom) {
+      dom.style.animationPlayState = 'running'
+    }
+  }
+
+  /**
+   * audio暂停
+   */
+  const audioPause = () => {
+    console.log('audioPause ', audio.value)
+    audio.value?.pause()
+    const dom: HTMLDivElement | null = document.querySelector('.music-img')
+    if (dom) {
+      dom.style.animationPlayState = 'paused'
+    }
+  }
+
+  /**
+   * 获取音乐详情
+   */
+  const getMusicDetail = () => {
+    const musicStore = useMusicStore()
+    const appStore = useAppStore()
+    musicApi.detail({ ids: musicStore.nowMusic?.id }).then((res: any) => {
+      // 错误处理返回异常
+      if (!res) {
+        return
+      }
+      const data = res.songs[0]?.al?.picUrl
+      musicStore.supplementMusic({
+        id: musicStore.nowMusic?.id,
+        cover: data,
+      })
+      getImgColor(data).then((res: any) => {
+        appStore.setMainColor(res)
+        appStore.setDarkColor(getDarkColor(res))
+      })
+    })
+  }
+  /**
+   * 获取音乐歌词
+   */
+  const getMusicLyric = () => {
+    const musicStore = useMusicStore()
+    musicApi.getLyric({ id: musicStore.nowMusic?.id }).then((res: any) => {
+      if (!res) {
+        return
+      }
+      const { lyric } = formatMusicLyrics(res.lrc.lyric)
+      musicStore.supplementMusic({
+        id: musicStore.nowMusic?.id,
+        lyric,
+      })
+    })
+  }
+  /**
+   * 获取音乐url
+   */
+  const getMusicUrl = (id: string) => {
+    const appStore = useAppStore()
+    const musicStore = useMusicStore()
+    getImgColor(musicStore.nowMusic?.cover || '').then((res: any) => {
+      appStore.setMainColor(res)
+      appStore.setDarkColor(getDarkColor(res))
+    })
+
+    const promise = new Promise((resolve, reject) => {
+      musicApi.getMusicUrl({ id }).then((res: any) => {
+        if (res.code !== 200) {
+          reject(res)
+          return
+        }
+        createAudio(res.data[0].url)
+        musicStore.supplementMusic({
+          id: musicStore.nowMusic?.id,
+          src: res.data[0].url,
+        })
+        resolve(res)
+      })
+    })
+    return promise
+  }
+
+  /**
+   * 搜索音乐
+   */
+  const getMusicSearch = (keywords: string) => {
+    const musicStore = useMusicStore()
+    musicApi.search({ keywords }).then((res: any) => {
+      const data = res.result.songs[0]
+      musicStore.pushPlayList({
+        id: data.id,
+        name: data.name,
+        singer: data.artists[0].name,
+        album: data.album.name,
+      })
+      // 优化
+      Promise.all([getMusicDetail(), getMusicLyric(), getMusicUrl(data.id)])
+        .then((res) => {
+          // console.log('Promise res', res)
+        })
+        .catch((err) => {
+          // console.log('Promise err', err)
+        })
+    })
+  }
   return {
     createAudio,
     createTimeupdate,
     audio,
     audioPlay,
     audioPause,
-  }
-}
-/**
- *
- *  创建audio
- * @param {string} src
- * @returns
- * @memberof Audio
- */
-const createAudio = (src: string) => {
-  // 清空之前的audio
-  if (audio.value) {
-    audio.value.removeEventListener('ended', audioEnded)
-    audio.value.removeEventListener('timeupdate', timeupdate)
-    audio.value.oncanplay = null
-    audio.value.pause()
-    audio.value = null
-  }
-  audio.value = new Audio(src)
-  // 音乐无法播放的情况
-  audio.value.onerror = () => {
-    const musicStore = useMusicStore()
-    useToast().open('音乐无法播放')
-    // 下一首
-    setTimeout(() => {
-      if (musicStore.musicList.length > 1) {
-        useToast().open('音乐无法播放，自动播放下一首')
-        musicStore.nextMusic()
-      }
-    }, 2500)
-  }
-  audio.value.addEventListener('ended', audioEnded)
-  audio.value.addEventListener('timeupdate', timeupdate)
-  audio.value.oncanplay = () => {
-    audioPlay()
-  }
-}
-/**
- * createTimeupdate
- * @param {Function} fn
- */
-const createTimeupdate = (fn: EventListenerOrEventListenerObject) => {
-  timeupdate = fn
-}
-
-/**
- * 音频播放结束时触发
- */
-const audioEnded = () => {
-  // audioPause()
-  const musicStore = useMusicStore()
-  switch (musicStore.playMode) {
-    case 0:
-      // 顺序
-      musicStore.nextMusic()
-      break
-    case 1:
-      // 随机
-      const randomIndex = Math.floor(Math.random() * musicStore.musicList.length)
-      // 防止随机到当前音乐
-      if (randomIndex === musicStore.nowIndex) {
-        musicStore.nextMusic()
-        break
-      }
-      musicStore.changeIndex(randomIndex)
-      break
-    case 2:
-      // 单曲循环
-      audioPlay()
-      break
-    default:
-      break
-  }
-}
-
-/**
- * audio播放
- */
-const audioPlay = () => {
-  audio.value?.play()
-  const dom: HTMLDivElement | null = document.querySelector('.music-img')
-  if (dom) {
-    dom.style.animationPlayState = 'running'
-  }
-}
-
-/**
- * audio暂停
- */
-const audioPause = () => {
-  console.log('audioPause ', audio.value)
-  audio.value?.pause()
-  const dom: HTMLDivElement | null = document.querySelector('.music-img')
-  if (dom) {
-    dom.style.animationPlayState = 'paused'
+    getMusicSearch,
+    getMusicUrl,
   }
 }
