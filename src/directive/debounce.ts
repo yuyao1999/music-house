efe4a148c0e4439835cf97c393702a9d26f59d16
王耀 <93106545+yy1486578543@users.noreply.github.com>
2024-02-02 09:45:51 +0800

    feat: 自定义指令 v-debounce

--- src/directive/debounce.ts
+++ src/directive/debounce.ts
@@ -1,19 +1,36 @@
-import { DirectiveBinding } from 'vue'
-const debounce = {
-  inserted: (el: any, binding: DirectiveBinding) => {
-    // 没有绑定函数抛出错误
+import type { Directive, DirectiveBinding } from 'vue'
+interface ElType extends HTMLElement {
+  __handleClick__: () => any
+}
+const debounce: Directive = {
+  mounted(el: ElType, binding: DirectiveBinding) {
     if (typeof binding.value !== 'function') {
-      throw 'debounce callback not a function'
+      throw 'callback must be a function'
     }
-
-    let timer: any
-    el.addEventListener('click', () => {
+    let timer: NodeJS.Timeout | null = null
+    let immediate = true
+    const wait = 500
+    const handleClick = () => {
       if (timer) clearTimeout(timer)
-
-      timer = setTimeout((_) => {
-        binding.value()
-      }, 1000)
-    })
+      if (immediate) {
+        let callNow = !timer
+        timer = setTimeout(() => {
+          timer = null
+        }, wait)
+        if (callNow) {
+          binding.value()
+        }
+      } else {
+        timer = setTimeout(() => {
+          binding.value()
+        }, wait)
+      }
+    }
+    el.__handleClick__ = handleClick
+    el.addEventListener('click', el.__handleClick__)
+  },
+  beforeUnmount(el: ElType) {
+    el.removeEventListener('click', el.__handleClick__)
   },
 }
 
